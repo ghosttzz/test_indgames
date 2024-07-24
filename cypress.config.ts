0ca865b15766fb674f63ff54c91f32ed33eded0e
Victor Eduardo <victor.pavesi@lincros.com>
2024-07-24 10:16:43 -0300

    finalizado

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     // e2e options here
-    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/integration/**/*.spec.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/index.ts',
+    baseUrl: 'https://slotcatalog.com/en/slots/'
   },
 })
